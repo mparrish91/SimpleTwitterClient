3cf8709f1ffd17d93c76b3635e55a5b5a7d47181
parry <parry@parrys-air.attlocal.net>
2016-10-30 22:48:38 -0700

    setting detailView

--- SimpleTwitterClient/STTweetDetailView.m
+++ SimpleTwitterClient/STTweetDetailView.m
@@ -7,6 +7,7 @@
 //
 
 #import "STTweetDetailView.h"
+#import "STTweet.h"
 
 @implementation STTweetDetailView
 
@@ -17,5 +18,40 @@
     // Drawing code
 }
 */
+
+
+- (instancetype)initWithTweet:(STTweet *)tweet
+{
+    if (!(self = [super init]))
+        return nil;
+    
+    self = [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class])
+                                          owner:self
+                                        options:nil] objectAtIndex:0];
+    self.retweetLabel.text = [tweet retweetName];
+    self.nameLabel.text = [tweet accountName];
+    self.usernameLabel.text = [NSString stringWithFormat:@"@%@",[tweet userName]];
+    self.timeLabel.text = [self convertDate:[tweet timestamp]];
+    self.tweetTextLabel.text = [tweet text];
+    self.retweetCountLabel.text = [NSString stringWithFormat:@"@%@",[tweet retweetCount]];
+    self.favoriteCountLabel.text = [NSString stringWithFormat:@"@%@",[tweet favoritesCount]];
+
+    
+        
+    return self;
+}
+
+
+- (NSString *)convertDate:(NSDate *)date
+{
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"dd/mm/yy,HH:mm Z y"];
+    NSString *resultString = [dateFormat stringFromDate:[NSDate date]];
+
+    
+    return resultString;
+}
+
+
 
 @end
