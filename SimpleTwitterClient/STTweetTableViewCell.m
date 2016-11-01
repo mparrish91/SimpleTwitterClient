763bb99c24df045be67d2c88771cd837b1d4f9a7
parry <parry@parrys-air.attlocal.net>
2016-10-31 21:45:43 -0700

    changing color

--- SimpleTwitterClient/STTweetTableViewCell.m
+++ SimpleTwitterClient/STTweetTableViewCell.m
@@ -18,16 +18,27 @@
     self.profilePhotoImageView.layer.cornerRadius = 15;
     self.profilePhotoImageView.clipsToBounds = YES;
 }
-- (IBAction)onReplyButtonPressed:(id)sender {
+- (IBAction)onReplyButtonPressed:(UIButton *)sender {
+    sender.selected = ![sender isSelected];
+
     NSString *id = self.tweet.id;
 
 }
 
-- (IBAction)onRetweetButtonPressed:(id)sender {
+- (IBAction)onRetweetButtonPressed:(UIButton *)sender {
+    sender.selected = ![sender isSelected];
 
 }
 
-- (IBAction)onFavoriteButtonPressed:(id)sender {
+- (IBAction)onFavoriteButtonPressed:(UIButton *)sender {
+    
+    
+        sender.selected = ![sender isSelected];
+
+//    [sender setSelected:YES];
+//    [sender setImage:[UIImage imageNamed: @"twitter_like_on"] forState:UIControlStateNormal];
+//    [sender setImage:[UIImage imageNamed: "twitter_like_on"] forState:UIControlStateSelected];
+//    [sender setImage:[UIImage imageNamed: "twitter_like_on"] forState:UIControlStateHighlighted ];
 
 }
 
